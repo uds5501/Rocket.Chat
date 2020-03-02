f4bad709790f6ac01bfb1c57b8ea68991b673fb4
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-03-02 18:04:01 -0300

    Move body definition from main template to App component

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 
 import { MeteorProvider } from '../providers/MeteorProvider';
 import { useReactiveValue } from '../hooks/useReactiveValue';
 
 export function App({ getPortals }) {
 	const portals = useReactiveValue(getPortals);
+
+	useLayoutEffect(() => {
+		document.body.classList.add('global-font-family', 'color-primary-font-color');
+
+		return () => {
+			document.body.classList.remove('global-font-family', 'color-primary-font-color');
+		};
+	}, []);
+
+	useLayoutEffect(() => {
+		const initialPageLoadingElement = document.getElementById('initial-page-loading');
+
+		if (!initialPageLoadingElement) {
+			return;
+		}
+
+		initialPageLoadingElement.style.display = 'none';
+
+		return () => {
+			initialPageLoadingElement.style.display = 'flex';
+		};
+	}, []);
 
 	return <MeteorProvider>
 		<div id='alert-anchor' />
